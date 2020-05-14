25605fa2b403fc7f0cd29b36799f24d70e4bb518
fp2000 <fp11233@gmail.com>
2020-05-14 14:25:21 +0200

    szad

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.use(bodyParser.json());
 app.use(methodOverride());
 app.use('/uploads', express.static('.profilePictures'))
 
-//app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 
 
